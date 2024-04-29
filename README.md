ad1c38795869a512063d249a90765a73a0bd224c
AlexandreNP9 <peder@alunos.utfpr.edu.br>
2024-04-29 20:45:36 -0300

    princípio de responsabilidade única

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # EngenhariaSoftware
 Repositório para atividades da disicplina de Engenharia de Software
+
+Utilizei exemplos em JavaScript, com a temática de Casa, para exemplificar 4 princípios do SOLID+2.
+
+single_responsibility_principle.js
+    A função adicionarEquipamento, da classe Comodo, tem apenas a função de adicionar o equipamento à lista de equipamentos daquele cômodo.
+
+    A função adicionarComodo, da classe Casa, tem apenas a função de adicionar o cômodo à lista de cômodos daquela casa.
+
+    A função ligarTodosEquipamentos, da classe Casa, tem a função de percorrer casa cômodo daquela casa. Em casa loop, vai chamar a função ligarEquipamento daquele cômodo.
+
---
+++ solid2p/single_responsibility_principle.js
@@ -0,0 +1,60 @@
+// classe casa com lista de cômodos
+class Casa {
+    constructor() {
+      this.comodos = [];
+    }
+  
+    // método adicionar cômodo à casa
+    adicionarComodo(comodo) {
+      this.comodos.push(comodo);
+    }
+  
+    // método ligar equipamentos de todos os cômodos da casa
+    ligarTodosEquipamentos() {
+      this.comodos.forEach(comodo => comodo.ligarEquipamentos());
+    }
+  }
+  
+  // classe comodo com lista de equipamentos
+  class Comodo {
+    constructor() {
+      this.equipamentos = [];
+    }
+  
+    // método adicionar equipamento ao cômodo
+    adicionarEquipamento(equipamento) {
+      this.equipamentos.push(equipamento);
+    }
+  
+    // método ligar todos os equipamentos do cômodo
+    ligarEquipamentos() {
+      this.equipamentos.forEach(equipamento => equipamento.ligar());
+    }
+  }
+  
+  // classe televisao
+  class Televisao {
+    ligar() {
+      console.log("Televisão ligada");
+    }
+  }
+  
+  // classe fogao
+  class Fogao {
+    ligar() {
+      console.log("Fogão ligado");
+    }
+  }
+  
+
+  const minhaCasa = new Casa();
+  const salaDeEstar = new Comodo();
+  salaDeEstar.adicionarEquipamento(new Televisao());    // a função adicionarEquipamento utiliza o príncípio de responsabilidade única
+  minhaCasa.adicionarComodo(salaDeEstar);               // a função adicionarComodo utiliza o príncípio de responsabilidade única
+  
+  const cozinha = new Comodo();
+  cozinha.adicionarEquipamento(new Fogao());
+  minhaCasa.adicionarComodo(cozinha);
+  
+  minhaCasa.ligarTodosEquipamentos();                   // a função ligarTodosEquipamentos também utiliza o príncípio de responsabilidade única, uma vez que seu escopo trabalha com foreach
+  
