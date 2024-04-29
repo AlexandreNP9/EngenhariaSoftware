fc1ff9ac847214be137ec97c23ba4ada1cdb6063
AlexandreNP9 <peder@alunos.utfpr.edu.br>
2024-04-29 20:56:18 -0300

    princípio aberto-fechado

--- README.md
+++ README.md
@@ -4,9 +4,16 @@ Repositório para atividades da disicplina de Engenharia de Software
 Utilizei exemplos em JavaScript, com a temática de Casa, para exemplificar 4 princípios do SOLID+2.
 
 single_responsibility_principle.js
+    Esse princípio diz que uma classe deve ter apenas uma razão para mudar. Isso deixa o código mais fácil de entender, porque cada classe deve se preocupar em representar apenas um tipo de elemento.
+
     A função adicionarEquipamento, da classe Comodo, tem apenas a função de adicionar o equipamento à lista de equipamentos daquele cômodo.
 
     A função adicionarComodo, da classe Casa, tem apenas a função de adicionar o cômodo à lista de cômodos daquela casa.
 
     A função ligarTodosEquipamentos, da classe Casa, tem a função de percorrer casa cômodo daquela casa. Em casa loop, vai chamar a função ligarEquipamento daquele cômodo.
+
+open_closed_principle.js
+    Esse princípio diz que as entidades devem estar abertas para extensão, mas fechadas para alteração.
+
+    Aqui foi adicionado uma classe ArCondicionado ao quarto, sem alterar o restante do código.
 
---
+++ open_closed_principle.js
@@ -0,0 +1,64 @@
+class Casa {
+    constructor() {
+      this.comodos = [];
+    }
+  
+    adicionarComodo(comodo) {
+      this.comodos.push(comodo);
+    }
+  
+    ligarTodosEquipamentos() {
+      this.comodos.forEach(comodo => comodo.ligarEquipamentos());
+    }
+  }
+  
+  class Comodo {
+    constructor() {
+      this.equipamentos = [];
+    }
+  
+    adicionarEquipamento(equipamento) {
+      this.equipamentos.push(equipamento);
+    }
+  
+    ligarEquipamentos() {
+      this.equipamentos.forEach(equipamento => equipamento.ligar());
+    }
+  }
+  
+  class Televisao {
+    ligar() {
+      console.log("Televisão ligada");
+    }
+  }
+  
+  class Fogao {
+    ligar() {
+      console.log("Fogão ligado");
+    }
+  }
+  
+  // nova classe
+  class ArCondicionado {
+    ligar() {
+      console.log("Ar condicionado ligado");
+    }
+  }
+  
+  
+  const minhaCasa = new Casa();
+  const salaDeEstar = new Comodo();
+  salaDeEstar.adicionarEquipamento(new Televisao());
+  minhaCasa.adicionarComodo(salaDeEstar);
+  
+  const cozinha = new Comodo();
+  cozinha.adicionarEquipamento(new Fogao());
+  minhaCasa.adicionarComodo(cozinha);
+  
+  // nova instância ArCondicionado adicionada sem modificar o código existente
+  const quarto = new Comodo();
+  quarto.adicionarEquipamento(new ArCondicionado());
+  minhaCasa.adicionarComodo(quarto);
+  
+  minhaCasa.ligarTodosEquipamentos();
+  
